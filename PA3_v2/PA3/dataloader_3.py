d57401c62907dd7e289ab50a5fc9e2441e8d8850
y8han <y8han@eng.ucsd.edu>
2021-02-13 19:59:22 -0800

    update

---
+++ PA3_v2/PA3/dataloader_3.py
@@ -0,0 +1,170 @@
+from torch.utils.data import Dataset, DataLoader# For custom data-sets
+import torchvision.transforms as transforms
+import numpy as np
+from PIL import Image
+import torch
+import pandas as pd
+from collections import namedtuple
+import random
+
+n_class    = 27
+
+# a label and all meta information
+Label = namedtuple( 'Label' , [
+    'name'        , 
+    'level3Id'    , 
+    'color'       , 
+    ] )
+
+labels = [
+    #       name                     level3Id  color
+    Label(  'road'                 ,    0  , (128, 64,128)  ),
+    Label(  'drivable fallback'    ,    1  , ( 81,  0, 81)  ),
+    Label(  'sidewalk'             ,    2  , (244, 35,232)  ),
+    Label(  'non-drivable fallback',    3  , (152,251,152)  ),
+    Label(  'person/animal'        ,    4  , (220, 20, 60)  ),
+    Label(  'rider'                ,    5  , (255,  0,  0)  ),
+    Label(  'motorcycle'           ,    6  , (  0,  0,230)  ),
+    Label(  'bicycle'              ,   7  , (119, 11, 32)  ),
+    Label(  'autorickshaw'         ,   8  , (255, 204, 54) ),
+    Label(  'car'                  ,   9  , (  0,  0,142)  ),
+    Label(  'truck'                ,  10 ,  (  0,  0, 70)  ),
+    Label(  'bus'                  ,  11 ,  (  0, 60,100)  ),
+    Label(  'vehicle fallback'     ,  12 ,  (136, 143, 153)),  
+    Label(  'curb'                 ,   13 ,  (220, 190, 40)),
+    Label(  'wall'                 ,  14 ,  (102,102,156)  ),
+    Label(  'fence'                ,  15 ,  (190,153,153)  ),
+    Label(  'guard rail'           ,  16 ,  (180,165,180)  ),
+    Label(  'billboard'            ,   17 ,  (174, 64, 67) ),
+    Label(  'traffic sign'         ,  18 ,  (220,220,  0)  ),
+    Label(  'traffic light'        ,  19 ,  (250,170, 30)  ),
+    Label(  'pole'                 ,  20 ,  (153,153,153)  ),
+    Label(  'obs-str-bar-fallback' , 21 ,  (169, 187, 214) ),  
+    Label(  'building'             ,  22 ,  ( 70, 70, 70)  ),
+    Label(  'bridge/tunnel'        ,  23 ,  (150,100,100)  ),
+    Label(  'vegetation'           ,  24 ,  (107,142, 35)  ),
+    Label(  'sky'                  ,  25 ,  ( 70,130,180)  ),
+    Label(  'unlabeled'            ,  26 ,  (  0,  0,  0)  ),
+]
+
+class Resize(object):
+    def __init__(self, output_size): # the output size should be a tuple of exactly (1080, 1920)
+        self.output_size = output_size
+        self.imageResize = transforms.Resize(self.output_size, Image.BILINEAR)
+        self.labelResize = transforms.Resize(self.output_size, Image.NEAREST)
+    def __call__(self, sample):
+        image, label = sample
+        img = self.imageResize(image)
+        lab = self.labelResize(label)
+        return img, lab
+class MirrorFlip(object):
+    def __init__(self, probability = 0.5):
+        self.hFlip = transforms.functional.hflip
+        self.prob = probability
+    def __call__(self, sample):
+        image, label = sample
+        if random.random() > self.prob:
+            img = self.hFlip(image)
+            lab = self.hFlip(label)
+        else:
+            img = image
+            lab = label
+        return img, lab
+class Rotate(object):
+    def __init__(self, degree = 10):
+        self.degree = degree
+        self.rotation = transforms.functional.rotate
+    def __call__(self, sample):
+        image, label = sample
+        imageDegree = self.degree * random.random() - self.degree/2
+        img = self.rotation(image, angle=imageDegree, resample=Image.BILINEAR)
+        lab = self.rotation(label, angle=imageDegree, resample=Image.NEAREST)
+        return img, lab
+
+class RondomCrop(object):
+    def __init__(self, shape):
+        self.shape = shape
+    def __call__(self, sample):
+        image, label = sample
+        a, b, c, d = transforms.RandomCrop.get_params(image, output_size=self.shape)
+        img = transforms.functional.crop(image, a, b, c, d)
+        lab = transforms.functional.crop(label, a, b, c, d)
+        return img,lab
+    
+class Blur(object):
+    def __init__(self, kernel_size = 3, sigma=(0.1, 1)):
+        self.kernel_size = kernel_size
+        self.sigma = sigma
+        self.blurr = transforms.GaussianBlur(self.kernel_size, self.sigma)
+    def __call__(self, sample):
+        image, lab = sample
+        self.blurr(image)
+        return image, lab
+    
+class ToTensor(object):
+    def __init__(self):
+        self.transform = transforms.ToTensor()
+    def __call__(self, sample):
+        image, label = sample
+        image = self.transform(image)
+        label = torch.from_numpy(np.asarray(label).copy()).long()
+        return image, label
+
+class CenterCrop(object):
+    def __init__(self):
+        pass
+    def __call__(self):
+        pass
+class Normalize(object):
+    def __init__(self):
+        self.transform = transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    def __call__(self, sample):
+        image, label = sample
+        image = self.transform(image)
+        return image, label
+
+class IddDataset(Dataset):
+    def __init__(self, csv_file, n_class=n_class, transforms_=None):
+        self.data      = pd.read_csv(csv_file)
+        self.n_class   = n_class
+        self.mode = csv_file
+        
+        # Add any transformations here
+        
+        # The following transformation normalizes each channel using the mean and std provided
+        self.transforms = transforms.Compose([
+                                            Resize((1024, 1920)),
+                                              #RondomCrop((1024, 1320)),
+                                              #Blur(),
+                                              #Rotate(),
+                                              #MirrorFlip(),
+                                              ToTensor(),
+                                              #Normalize(),
+                                             ])
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        
+        img_name = self.data.iloc[idx, 0]
+        img = Image.open(img_name).convert('RGB')
+        label_name = self.data.iloc[idx, 1]
+        label = Image.open(label_name)
+
+        img, label = self.transforms((img, label)) # Normalization
+        #label = torch.from_numpy(label.copy()).long() # convert to tensor
+
+        # create one-hot encoding
+        label = torch.squeeze(label)
+        
+        #h, w = label.shape
+        #target = torch.zeros(self.n_class, h, w)
+        #for c in range(self.n_class):
+        #    target[c][c==label] = 1
+        
+        return img, label
+
+
+
+
--- PA3_v2/PA3/starter.py
+++ PA3_v2/PA3/starter.py
@@ -1,7 +1,7 @@
 from torchvision import utils
 from basic_fcn import *
-from dataloader import *
-from utils import *
+from dataloader_3 import *
+# from utils import *
 import torchvision
 import torch.nn.functional as F
 import torch.optim as optim
@@ -10,28 +10,32 @@ import time
 
 
 # TODO: Some missing values are represented by '__'. You need to fill these up.
-
+Batch_size = 4
 train_dataset = IddDataset(csv_file='train.csv')
+batch_train = DataLoader(train_dataset, batch_size = Batch_size, num_workers = 2)
 val_dataset = IddDataset(csv_file='val.csv')
 test_dataset = IddDataset(csv_file='test.csv')
 
 
-train_loader = DataLoader(dataset=train_dataset, batch_size= __, num_workers= __, shuffle=True)
-val_loader = DataLoader(dataset=val_dataset, batch_size= __, num_workers= __, shuffle=True)
-test_loader = DataLoader(dataset=test_dataset, batch_size= __, num_workers= __, shuffle=False)
+# train_loader = DataLoader(dataset=train_dataset, batch_size= __, num_workers= __, shuffle=True)
+# val_loader = DataLoader(dataset=val_dataset, batch_size= __, num_workers= __, shuffle=True)
+# test_loader = DataLoader(dataset=test_dataset, batch_size= __, num_workers= __, shuffle=False)
 
 
 def init_weights(m):
     if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
-        torch.nn.init.xavier_uniform(m.weight.data)
-        torch.nn.init.xavier_uniform(m.bias.data)        
+        # Kernal parameters are learnable
+        torch.nn.init.xavier_uniform_(m.weight.data)
+        torch.nn.init.zeros_(m.bias.data)
 
-# epochs = __
-# criterion = # Choose an appropriate loss function from https://pytorch.org/docs/stable/_modules/torch/nn/modules/loss.html
-fcn_model = FCN(n_class=n_class)
+epochs = 50   # Trainging Epoch
+criterion = torch.nn.CrossEntropyLoss(ignore_index = n_class)  # Choose an appropriate loss function from https://pytorch.org/docs/stable/_modules/torch/nn/modules/loss.html
+# TODO: ignore index out of boundry (0-26, but 27,28 may appear)
+
+fcn_model = FCN(n_class = n_class)
 fcn_model.apply(init_weights)
 
-optimizer = optim.Adam(fcn_model.parameters(), lr=__)
+optimizer = optim.Adam(fcn_model.parameters(), lr=0.9)
 
 use_gpu = torch.cuda.is_available()
 if use_gpu:
@@ -42,17 +46,50 @@ def train():
     fcn_model.train()
     for epoch in range(epochs):
         ts = time.time()
-        for iter, (X, tar, Y) in enumerate(train_loader):
+        # For test (one image)
+        # for i in range(100):
+        #     optimizer.zero_grad()
+        #     tmp_a = train_dataset[i][0].shape[0]
+        #     tmp_b = train_dataset[i][0].shape[1]
+        #     tmp_c = train_dataset[i][0].shape[2]
+        #     X = train_dataset[i][0].reshape(1, tmp_a, tmp_b, tmp_c)
+        #     X = X
+        #     outputs = fcn_model.forward(X)
+        #     print(outputs.shape)
+        #     test = outputs[0]
+        #     print(train_dataset[i][2].shape)
+        #     print(test.shape)
+        #     print(test.reshape(1024,n_class,1920).shape)
+        #     print(torch.max(train_dataset[i][2]))
+        #     print(torch.min(train_dataset[i][2]))
+        #     loss = criterion(test.reshape(1024,n_class,1920), train_dataset[i][2])
+        #     print(loss)
+        #     loss.backward()
+        #     optimizer.step()
+        # For test (one image)
+        for iter, (X, Y) in enumerate(batch_train):
+            # each iter bag contains a batch size of images and labels
+            # X is the input  size: N,H,W
+            # tar is the target size: N,n_class,H,W
+            # Y is label (compute cross entropy loss)
             optimizer.zero_grad()
-
             if use_gpu:
-                inputs = # Move your inputs onto the gpu
-                labels = # Move your labels onto the gpu
+                inputs = X.to('cuda') # Move your inputs onto the gpu
+                labels = Y.to('cuda') # Move your labels onto the gpu
             else:
-                inputs, labels = # Unpack variables into inputs and labels
-
-            outputs = fcn_model(inputs)
+                inputs, labels = X, Y # Unpack variables into inputs and labels
+            # print(inputs.shape)
+            outputs = fcn_model.forward(inputs)
+            # Output size: N,n_class.H,W
+            # print(outputs.shape)
+            # print(labels.shape)
+            # print(labels[0])
+            # print(torch.max(labels[0]))
+            # print(torch.max(labels[1]))
+            # print(torch.max(labels[2]))
+            # print(torch.max(labels[3]))
             loss = criterion(outputs, labels)
+            # print(loss)
             loss.backward()
             optimizer.step()
 
@@ -78,6 +115,5 @@ def test():
     # Make sure to include a softmax after the output from your model
     
 if __name__ == "__main__":
-    print(use_gpu)
-    val(0)  # show the accuracy before training
+    # val(0)  # show the accuracy before training
     train()
